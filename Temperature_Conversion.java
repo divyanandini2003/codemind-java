415cc2392ebeaa4cfb6f8324425fbeb45db360b5
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2023-01-31 12:11:28 +0530

    import java.util.*; class javaa {     public static void main(String aargs[])     {         Scanner sc =new Scanner(System.in);         int c=sc.nextInt();         float F=(float)(32+(c*1.8));         System.out.format("%.2f",F);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class javaa
+{
+    public static void main(String aargs[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int c=sc.nextInt();
+        float F=(float)(32+(c*1.8));
+        System.out.format("%.2f",F);
+    }
+}
